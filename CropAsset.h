ab504bcaf8a45aa488bee28bd3eb76fb2008dab5
shreyajp <shreyajp6@gmail.com>
2024-10-15 20:38:08 +1030

    edits to crop asset function

--- CropAsset.h
+++ CropAsset.h
@@ -2,35 +2,35 @@
 #define CROPASSET_H
 
 #include "FarmAsset.h"
-#include <string>  // Include string for std::string
+#include <string> 
 
 class CropAsset : public FarmAsset {
 protected:
     int growthStage;
-    int maxGrowthStage;  // Max stage when the crop is fully grown
+    int maxGrowthStage;  //sets maximum for crops to grow
     bool isHarvestable;
 
 public:
-    // Constructor with maxGrowth as a parameter
+    // constructor with maxGrowth as a parameter
     CropAsset(float value, std::string cropName, int maxGrowth = 3);
 
-    // Increment the growth stage
+    // increments growth stage
     virtual void grow();
 
-    // Check if crop is harvestable
+    // check if crop is harvestable
     virtual bool getIsHarvestable() const;
 
-    // Setter for growth stage
+    // setter for growth stage
     virtual void setGrowthStage(int newGrowthStage);
 
-    // Get the current growth stage
+    // get current growth stage
     virtual int getGrowthStage() const;
 
-    // Get the maximum growth stage
+    // get maximum growth stage
     virtual int getMaxGrowthStage() const;
 
-    // Pure virtual function to be implemented by derived classes
-    virtual void produce() override = 0;  // Derived classes should implement how they grow
+    // virtual function, used by derived classes
+    virtual void produce() override = 0;  // derived classes implement how they grow
 };
 
 #endif  // CROPASSET_H
