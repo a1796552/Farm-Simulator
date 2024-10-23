407a6a99a312369fd9c2d25fae3724f78847104e
Shreya JP <shreyajp@Shreyas-MacBook-Pro.local>
2024-10-24 10:22:31 +1030

    new

--- Robot.hpp
+++ Robot.hpp
@@ -1,9 +1,40 @@
+#ifndef ROBOT_HPP
+#define ROBOT_HPP
+
 #include "GridItem.hpp"
 
+class Robot : public GridItem {
+private:
+    int health;
 
-int Robot(int gridWidth, int gridHeight);
+public:
+    Robot(int gridWidth, int gridHeight) : GridItem(0, 0, gridWidth, gridHeight), health(3) {}
 
+    int getHealth() const {
+        return health;
+    }
 
-int getHealth();
-void takeHit();
-bool move(int xOffset, int yOffset);
+    void takeHit() {
+        if (health > 0) {
+            health--;
+        }
+    }
+
+    bool move(int xOffset, int yOffset) {
+        if ((xOffset != 0 && yOffset != 0) || (xOffset == 0 && yOffset == 0)) {
+            return false;
+        }
+
+        int newX = getCoordinates().first + xOffset;
+        int newY = getCoordinates().second + yOffset;
+
+        if (newX >= 0 && newX < getGridWidth() && newY >= 0 && newY < getGridHeight()) {
+            setCoordinates(newX, newY);
+            return true;
+        }
+
+        return false;
+    }
+};
+
+#endif
