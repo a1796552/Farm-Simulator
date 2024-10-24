8ed0aa507effa8418bbc082f6890cc008eda484e
Shreya JP <shreyajp@Shreyas-MacBook-Pro.local>
2024-10-24 10:38:08 +1030

    new

--- Obstacle.hpp
+++ Obstacle.hpp
@@ -1,10 +1,20 @@
-#ifndef INTERACTABLE_HPP
-#define INTERACTABLE_HPP
+#ifndef OBSTACLE_HPP
+#define OBSTACLE_HPP
 
-#include "GridItem.hpp"
-#include "Robot.hpp"
+#include "Interactable.hpp"
 
-enum InteractableType {
-    GOAL,
-    OBSTACLE
+class Obstacle : public Interactable {
+public:
+    Obstacle(int x, int y, int width, int height) : Interactable(x, y, width, height) {}
+
+    void interact(Robot* player) override {
+        player->takeHit();
+        std::cout << "Hit obstacle! Robot health: " << player->getHealth() << std::endl;
+    }
+
+    InteractableType getType() const override {
+        return OBSTACLE;
+    }
 };
+
+#endif 
